47b360effec90191d085db991ef78c96ded9886c
zhaozongzhao <2206321864@qq.com>
2019-04-03 20:06:34 +0800

    python 可安装的包

--- pytest_practice/some_module_proj/setup.py
+++ pytest_practice/some_module_proj/setup.py
@@ -1,13 +1,23 @@
 # python 分发工具setuptools 能帮助我们更好的创建很分发
 from setuptools import setup
 
+#通过python pip安装
+# setup(
+#     name = 'some_module',  #应用名
+#     version='0.0.1', # 版本号
+#     py_modules = ['some_module'],
+#     url = 'None',
+#     author = 'zzz',
+#     author_email = '3031371046@qq.com',
+#     # packages = ['some_module']
+#     # #  安装包内的python模块
+# )
+
+from setuptools import setup,find_packages
+
 setup(
-    name = 'some_module',  #应用名
-    version='0.0.1', # 版本号
-    py_modules = ['some_module'],
-    url = 'None',
-    author = 'zzz',
-    author_email = '3031371046@qq.com',
-    # packages = ['some_module']
-    # #  安装包内的python模块
+    name = 'some_package',
+    packages = find_packages(where = 'src'),
+    package_dir = {'':'src'}
+
 )
---
+++ pytest_practice/some_module_proj/src/some_package/__init__.py
@@ -0,0 +1,1 @@
+from pytest_practice.some_module_proj.src.some_package.some_module import *
